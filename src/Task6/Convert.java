3d4d08ed25d48874fe1b8aa3078972812a3abb33
asoltysh <asoltysh@gmail.com>
2021-09-30 13:46:02 +0300

    Задача №6_ Операторы Напишите программу конвертер физических величин:
    1. Пользователю предлагается на выбор ввести массу или расстояние. Пример:
    > Выберите что переводить: 1 - масса, 2 - расстояние
    > 2
    
    2. Пользователю предлагается выбрать единицу измерения. Пример:
    > Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут
    > 1
    
    3. Пользователю предлагается ввести количество выбранных единиц:
    > Введите число
    > 10
    > Результат:
    > Метры: 10
    > Мили: 0.006
    > Ярды: 10.94
    > Футы: 32.81

---
+++ src/Task6/Convert.java
@@ -0,0 +1,21 @@
+package Task6;
+
+import java.util.Scanner;
+
+public class Convert {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
+        int a = scanner.nextInt();
+
+        if (a == 1){
+            System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+            int b = scanner.nextInt();
+
+        } else if (){
+
+        }
+    }
+
+
+}

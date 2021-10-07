ba5a487e7f85348131320749537d3fe2cf73f980
asoltysh <asoltysh@gmail.com>
2021-10-07 18:40:28 +0300

    Задача №10_Циклы Напишите программу, где пользователь вводит данные с клавиатуры в матрицу, а после этого произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3. Размерность матрицы задается пользователем.
    Выполненное задания нужно залить в гит и отправить ссылку преподавателю на почту

---
+++ src/Task10/matrix.java
@@ -0,0 +1,32 @@
+package Task10;
+
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размерность матрицы:");
+
+        int a = scanner.nextInt();;
+        int c = scanner.nextInt();;
+
+        int[][] array = new int[a][c];
+
+        System.out.println("Введите данные в матрицу:");
+        for (int i = 0; i <  a; i++) {
+            for (int b = 0;  b < c; b++) {
+                array[i][b] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i <  a; i++) {
+            for (int b = 0;  b < c; b++) {
+                if (i == 0){
+                    array[i][b] *= 3;
+                }
+                System.out.print(array[i][b] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

a3cb944cd98ed07429d2b60d64cf4edfeaa4afe0
asoltysh <asoltysh@gmail.com>
2023-02-14 16:52:21 +0300

    - Реализовать сложение дробных чисел, которые пользователь вводит в консоль.
    - Использовать комментарии и JavaDoc при описании метода.
    
    - Использовать форматирование при выводе результата в консоль.
    
    Полученный результат округлять до 4-х знаков после запятой.
    
    Запушить проект в свой репозиторий на GitHub и прикрепить ссылку на него в ответе на задание
    
    * Доп задание по желание: добавить возможность выбора другой операции (сложение, вычитание, деление, умножение)

--- src/AdvancedTask1/Calculated.java
+++ src/AdvancedTask1/Calculated.java
@@ -3,6 +3,27 @@ package AdvancedTask1;
 import java.util.Scanner;
 
 public class Calculated {
+
+    public static void  sum(double c, double d) {
+        double result = c + d;
+        System.out.printf("Результат: %.4f", result);
+    }
+
+    public static void  minus(double c, double d) {
+        double result = c - d;
+        System.out.printf("Результат: %.4f", result);
+    }
+
+    public static void  multiplication(double c, double d) {
+        double result = c / d;
+        System.out.printf("Результат: %.4f", result);
+    }
+
+    public static void  division(double c, double d) {
+        double result = c * d;
+        System.out.printf("Результат: %.4f", result);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,6 +33,26 @@ public class Calculated {
         System.out.println("Введите вторую переменную: ");
         double b = scanner.nextDouble();
 
-        System.out.println(a + " " + b);
+        System.out.println("Введите операцию: + - * /");
+        char c = scanner.next().charAt(0);
+
+        switch (c) {
+            case '+':
+                sum(a,b);
+                break;
+            case '-':
+                minus(a,b);
+                break;
+            case '*':
+                division(a,b);
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("Нельзя делить на 0");
+                } else {
+                    multiplication(a,b);
+                }
+                break;
+        }
     }
 }
